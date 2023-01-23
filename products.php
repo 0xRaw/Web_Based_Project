fb397704efeb0896e41d61a52000fc88191a636f
0xRaw <r9eitchalhacker@gmail.com>
2023-01-23 07:53:30 +0300

    Retrevial From DB

--- products.php
+++ products.php
@@ -100,7 +100,38 @@
         <button type="submit"><i class="fas fa-search"></i></button>
         </form>
       </div>
-
+      <?php
+$query = "SELECT * FROM Books";
+$result = mysqli_query($conn, $query);?>
+<div class="row">
+    <?php 
+    $counter = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $bookName = $row['BookName'];
+        $bookAuthor = $row['BookAuthor'];
+        $bookPrice = $row['BookPrice'];
+        $bookImage =  str_replace(' ', '', $row['BookImage']); //to remove spaces in picture names
+        $bookCategory = $row['BookCategory'];
+        echo '<div class="col-4">';
+        echo "<a href='book-detail.html'>
+        <img src='bookimages/$bookImage' alt='test' /></a>";
+        echo '<h4>'.$bookName.'</h4>';
+        echo '<div class="rating">
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star-o"></i>
+      </div>';
+      echo '<p>'.$bookPrice.' SAR </p>';
+      echo '</div>';
+      $counter++;
+      if($counter % 4 == 0){
+            echo '</div><div class="row">';
+        }
+    }
+    ?>
+</div><!-- THIS IS THE ORIGINAL CODE (IF YOU NEED IT AS REFERCNE)
       <div class="row">
         <div class="col-4">
           <a href="book-detail.html">
@@ -116,143 +147,7 @@
           </div>
           <p>Rs.500</p>
         </div>
-        <div class="col-4">
-          <img src="images/Book 5.jpg" alt="Book 5" />
-          <h4>Watership Down</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 6.jpg" alt="Book 6" />
-          <h4>All The Night We Cannot See</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 7.jpg" alt="Book 7" />
-          <h4>The HOBBIT</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-4">
-          <img src="images/Book 8.jpg" alt="Book 8" />
-          <h4>Anna Karenina</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 9.jpg" alt="Book 9" />
-          <h4>Watership Down</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 10.jpg" alt="Book 10" />
-          <h4>All The Night We Cannot See</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 11.jpg" alt="Book 11" />
-          <h4>The HOBBIT</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-4">
-          <img src="images/Book 12.jpg" alt="Book 12" />
-          <h4>Anna Karenina</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 13.jpg" alt="Book 13" />
-          <h4>Watership Down</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 14.jpg" alt="Book 14" />
-          <h4>All The Night We Cannot See</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-        <div class="col-4">
-          <img src="images/Book 15.jpg" alt="Book 15" />
-          <h4>The HOBBIT</h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-          </div>
-          <p>Rs.500</p>
-        </div>
-      </div>
+         -->
 	  <!-- ---------------------Youtube Video------------------- -->
     <div class="youtube-container">
       <div class="youtube-row">
