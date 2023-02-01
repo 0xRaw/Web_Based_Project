50d6ad579debf1ff63ab5a481d589ae7d6f1d8a4
0xRaw <76697978+0xRaw@users.noreply.github.com>
2023-02-01 16:12:28 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ session_destroy();
 //what verifies a user on our side.. :)
 unset($_SESSION['username']);
 unset($_SESSION['userID']);
+setcookie("UserID", "", time() - 3600);
 header("Location: account.php");
 exit;
 ?>
